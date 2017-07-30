80be024e8d3164a7e399862a9925287f6fa992e6
Mikhail Metrikin <metrikin@gmail.com>
2017-07-29 20:42:05 -0700

    Init build configs

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
