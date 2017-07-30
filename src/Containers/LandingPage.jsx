0162ea318cd48c33cc264cc141e174e7b246e0c1
Mikhail Metrikin <metrikin@gmail.com>
2017-07-29 22:23:32 -0700

    Canvas background gradient

--- src/Containers/LandingPage.jsx
+++ src/Containers/LandingPage.jsx
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Col, Jumbotron, Button } from 'react-bootstrap';
+
+import Granim from 'granim';
 
 export default class LandingPage extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  componentDidMount() {
+    new Granim({
+      element: '#granim-canvas',
+      name: 'granim',
+      opacity: [1, 1],
+      states: {
+        'default-state': {
+          gradients: [['#2b5876', '#4e4376'], ['#1A2980', '#26D0CE']],
+          transitionSpeed: 15000
+        }
+      }
+    });
+  }
   render() {
     return (
       <div>
-        <Navbar />
-        <Jumbotron>
-          <Button>Test</Button>
-        </Jumbotron>
+        <canvas
+          id="granim-canvas"
+          style={{
+            position: 'absolute',
+            display: 'block',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: -1
+          }}
+        />
+        <span>
+          <p>TEST TEXT</p>
+        </span>
       </div>
     );
   }
